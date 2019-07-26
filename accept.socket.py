e48484e63616497a7ca1e89952f92e70b3281052
szk01 <1622391371@qq.com>
2019-07-26 17:40:13 +0800

    加上accept_socket

---
+++ accept.socket.py
@@ -0,0 +1,28 @@
+import socket
+
+def log(*args, **kwargs):
+    print(*args, **kwargs)
+
+
+def RecHttp(response):
+    ip = '192.168.101.39'         #局域网IP,路由器分配给电脑的，在cmd可以查询
+    port = 80
+    client = socket.socket()
+    address = (ip, port)
+    client.bind(address)
+
+    while True:
+        client.listen(5)
+        log('connect waiting...')
+        connection, address = client.accept()
+        request = connection.recv(1024)
+        log('connect sucessed...')
+        log('ip and request, {}\n{}'.format(address, request.decode('utf-8')))
+        #返回响应
+        connection.sendall(response)
+        connection.close()
+
+
+if __name__ == '__main__':
+    response = 'hello world!'
+    RecHttp(response)
