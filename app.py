52cc1d6b3feacb244d0ae4e43f27b8a2a743e513
小伯爵 <1622391371@qq.com>
2019-08-13 15:42:37 +0800

    435465

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import (
     Flask,
+    request,
 )
+import requests
 from flask_socketio import SocketIO
-
+from utils import log
 from routes.index import main as index_routes
 from routes.login import main as login_routes
-from routes.OM import main as OM_routes
 from routes.test import main as test_routes
+from allFuncs import Funcs
 
 # 先要初始化一个 Flask 实例，并将Flask-SocketIO添加到Flask应用程序
 app = Flask(__name__)
@@ -18,7 +20,43 @@ socketio = SocketIO(app)
 app.register_blueprint(index_routes, url_prefix='/index')
 app.register_blueprint(test_routes, url_prefix='/test')
 app.register_blueprint(login_routes, url_prefix='/login')
-app.register_blueprint(OM_routes, url_prefix='/OM')
+
+
+# 给OM服务器发送一个POST请求
+def reqestOM(body):
+    url = 'https://zhidi.sfyf.cn:1888/xml'
+    payload = body
+    headers = {
+        'content-type': 'text/xml',
+    }
+    requests.request("POST", url, data=payload, headers=headers, verify=False)
+
+
+# 接收客户端发送过来的消息，确认通道连接
+@socketio.on('phone')
+def send(data):
+    log('use webScoket receive sucessful', data)
+
+
+# 会使用到多线程，不同的进程处理不同的请求
+@app.route('/ip_phone', methods=['GET'])
+def ip_phone():
+    log(request.method)
+    xml = request.data  # 传过来的数据类型是byte类型
+    xml = xml.decode('utf-8')
+    # 开始处理各种请求
+    log('OM向应用服务器发送的请求数据:', xml)
+    # 将OM的请求数据清洗出来访者id
+    funct = Funcs(xml)
+    body = funct.funcs()
+    log('发送给OM的请求：', body)
+    # 接收到一个请求之后，发送一个请求
+    # 只要body不为空，说明有请求需要发送。判断一下请求会发送给OM还是js客户端
+    if len(body) == 13 and '1' in body:             # 如果是电话号码，发送给客户端
+        socketio.emit(event='number', data=body)
+    elif body is not None:
+        reqestOM(body)
+    return 'App Server sucess receive!'
 
 
 # 运行服务器
