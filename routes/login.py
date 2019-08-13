a0d50ce646a9288a6ed46260b79a068d67565ca9
小伯爵 <1622391371@qq.com>
2019-08-13 09:54:32 +0800

    测试login

--- routes/login.py
+++ routes/login.py
@@ -3,19 +3,23 @@ from flask import (
     render_template,
     Blueprint,
 )
-from models.user import User
+# from models.user import User
 
 main = Blueprint('login', __name__)
 
-# 做一个用户和密码的判断
-@main.route('/index', methods=['POST'])
-def index():
-    form = request.form
-    name = form['username']
-    pwd = form['password']
-    user = User.query.filter_by(username=name).first()
-    if user.password == pwd:
-        return render_template("index.html")        # 登录成功
-    else:
-        return render_template("login.html", status="用户名或者密码错误")    # 登录失败
+# # 做一个用户和密码的判断
+# @main.route('/index', methods=['POST'])
+# def index():
+#     form = request.form
+#     name = form['username']
+#     pwd = form['password']
+#     user = User.query.filter_by(username=name).first()
+#     if user.password == pwd:
+#         return render_template("index.html")        # 登录成功
+#     else:
+#         return render_template("login.html", status="用户名或者密码错误")    # 登录失败
 
+
+@main.route('/om-sigin', methods=['POST'])
+def index():
+    pass
