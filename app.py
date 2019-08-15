6744e4f2be398e09617e423d94bb688a6205e19b
小伯爵 <1622391371@qq.com>
2019-08-15 15:47:53 +0800

    0.0.0.0

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         debug=True,
         # host='127.0.0.1',
         # local_host='106.15.44.224',
-        # host='0.0.0.0',
+        host='0.0.0.0',
         # host='192.168.101.39',
         port=80,
         app=app,
