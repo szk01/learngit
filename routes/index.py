7ad707d2234034c48346dd38aef90b46d54a7eb4
Robert <18031577090@163.com>
2019-08-26 18:07:40 +0800

    433434

--- routes/index.py
+++ routes/index.py
@@ -9,34 +9,13 @@ from functools import wraps
 
 main = Blueprint('index', __name__)
 
-
-def login_required(func):
-    @wraps(func)
-    def qingwa(*args, **kwargs):
-        if session.get('user_id'):
-            return func(*args, **kwargs)
-        else:
-            return redirect(url_for('login'))
-
-    return qingwa
-
-
-@main.route('/index', methods=['GET'])
-@login_required
-def index():
-    if current_user.is_active:
-        return render_template('index.html')
-    else:
-        return redirect('/login')
-    return '成功'
-
 # 通话记录
 @main.route('/call_record')
-def record():
+def call_record():
     return render_template('callRecord.html')
 
 
 # 录音记录
 @main.route('/voice_record')
-def record():
+def voice_record():
     return render_template('voiceRecord.html')
