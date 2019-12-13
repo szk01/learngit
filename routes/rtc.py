c016034e71b2862748293074710f73b1499d75cb
Robert.su <18031577090@163.com>
2019-12-13 15:28:47 +0800

    24232

--- routes/rtc.py
+++ routes/rtc.py
@@ -21,14 +21,14 @@ main = Blueprint('rtc', __name__)
 
 
 parser = OptionParser()
-parser.add_option("-a", "--listen", dest="10030", help="Listen port")
+# parser.add_option("-a", "--listen", dest="listen", help="Listen port")
 parser.add_option("-d", "--appid", dest="zk46xnwd", help="ID of app")
 parser.add_option("-k", "--appkey", dest="dde07e3682c1ea002a70a2d7d743edbd", help="Key of app")
 parser.add_option("-e", "--gslb", dest="gslb", help="URL of GSLB")
 
 (options, args) = parser.parse_args()
-(listen, app_id, app_key, gslb) = (options.listen, options.appID, options.appKey, options.gslb)
-
+# (listen, app_id, app_key, gslb) = (options.listen, options.appID, options.appKey, options.gslb)
+(app_id, app_key, gslb) = (options.appID, options.appKey, options.gslb)
 '''
 客服端发送过来的参数有：
     
